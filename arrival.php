abe7c784dea997a99e2827ee66e65e58d5413bc3
Ketaki Padhye <ketaki.p@evergrowsolutions.com>
2022-05-10 12:47:00 +0530

    Submit

---
+++ arrival.php
@@ -0,0 +1,117 @@
+<?php include 'header.php';?>
+<!-- This is my first testing 2 -->
+<main>
+<section id="breadcrumbs">
+<div class="container-large"><a href="index.php"><img src="assets/images/icon-home__H18.svg" style="width: 18px;margin-top:-5px;" /></a> <span style="font-size:15px;">❯</span> <a class="current" href="arriving-information.html">Arriving Information</a></div>
+</section>
+
+<section class="sct-page-top-image" data-page="inner" data-slide-image="reasons-to-exhibit.jpg" style="background: url(https://packexindia.com/assets/images/inner/reasons-to-exhibit.jpg) center center / cover no-repeat;">
+<div class="container-large">
+<div class="page-teaser">
+<p>Arriving Information</p>
+</div>
+</div>
+</section>
+
+<section class="content1">
+<div class="container-large clearfix">
+<div class="row">
+<div class="col-md-8 feat-col">
+<div class="wrap content-box wow fadeInUp   animated" data-wow-delay="0.4s" style="visibility: visible; animation-delay: 0.4s;"><img alt="" src="https://anutecindia.com/assets/images/pragati-maidan.jpg" width="100%" /><iframe allowfullscreen="" frameborder="0" height="400" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3769.022289444566!2d72.85139011490232!3d19.150501587045117!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b7ca86e01c89%3A0xa987fb34838933c4!2sBombay%20Exhibition%20Centre%2C%20NESCO%2C%20Goregaon%2C%20Mumbai%2C%20Maharashtra%20400063!5e0!3m2!1sen!2sin!4v1638865651212!5m2!1sen!2sin" style="border:0" width="100%"></iframe><!--<iframe src="assets/images/embed.html" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen=""></iframe>--></div>
+</div>
+
+<div class="col-md-4 feat-col">
+<div class="wrap content-box wow fadeInUp   animated" data-wow-delay="0.4s" style="visibility: visible; animation-delay: 0.4s;">
+<p class="title3">Welcome to Mumbai!</p>
+
+<p>Bombay Exhibition Centre is ideally located within 10 minutes from the airport and a 20-minute drive from the heart of the city. Visitors can reach the venue quickly by public transport, metro, car, train and aeroplane from many cities in India and abroad.
+</p>
+&nbsp;
+
+<div class="row m-0">
+<div class="col-md-12">
+<div class="accordion" id="accordion-arrive-info">
+<div class="card">
+<div class="card-header" id="headingOne">
+<p class="mb-0"><strong><a aria-controls="collapseOne" aria-expanded="false" class="collapsed w-100" data-target="#collapseOne" data-toggle="collapse" type="button"> Arrival by Road </a></strong></p>
+</div>
+
+<div aria-labelledby="headingOne" class="collapse" data-parent="#accordion-arrive-info" id="collapseOne">
+<div class="card-body">
+<p>Bombay Exhibition Centre (BEC) is quickly and directly accessible by car from main highways. Commuting is a breeze with easy access to the Western Express Highway.
+<br>Reach the Ram Mandir Station in Mumbai.
+<br>Bombay Exhibition Centre is a 8 minute walk from the station. Use any public transport to get to the exhibition venue in few minutes.
+<br>For train ticket bookings,
+<br>visit: www.irctc.co.in</p>
+</div>
+</div>
+</div>
+
+<div class="card">
+<div class="card-header" id="headingTwo">
+<p class="mb-0"><strong><a aria-controls="collapseTwo" aria-expanded="false" class="collapsed w-100" data-target="#collapseTwo" data-toggle="collapse" type="button"> Arrival by Train </a></strong></p>
+</div>
+
+<div aria-labelledby="headingTwo" class="collapse" data-parent="#accordion-arrive-info" id="collapseTwo">
+<div class="card-body">Reach the Ram Mandir Station in Mumbai.<br />
+Bombay Exhibition Centre is a 8 minute walk from the station. Use any public transport to get to the exhibition venue in few minutes.
+For train ticket bookings,
+<br>
+visit: <a href="https://www.irctc.co.in/" target="_blank">www.irctc.co.in</a></div>
+</div>
+</div>
+
+<div class="card">
+<div class="card-header" id="headingFour">
+<p class="mb-0"><strong><a aria-controls="collapseFour" aria-expanded="false" class="collapsed w-100" data-target="#collapseFour" data-toggle="collapse" type="button"> Arrival by Aeroplane </a></strong></p>
+</div>
+
+<div aria-labelledby="headingFour" class="collapse" data-parent="#accordion-arrive-info" id="collapseFour">
+<div class="card-body">Chattrapathi Shivaji International Airport makes Mumbai easy and convenient to reach by air from all domestic and international cities. Use any public transport to reach the venue within minutes
+</div>
+</div>
+</div>
+
+<!--<div class="card">
+<div class="card-header" id="headingFive">
+<p class="mb-0"><strong><a aria-controls="collapseFive" aria-expanded="false" class="collapsed w-100" data-target="#collapseFive" data-toggle="collapse" type="button"> Delhi Metro </a></strong></p>
+</div>
+
+<div aria-labelledby="headingFive" class="collapse" data-parent="#accordion-arrive-info" id="collapseFive">
+<div class="card-body">The Delhi Metro is a mass rapid transit system serving Delhi and its satellite cities. Pragati Maidan Metro station on the Blue line is within walking distance from IECC, Pragati Maidan, New Delhi.</div>
+</div>
+</div>-->
+
+<div class="card">
+<div class="card-header" id="headingSix">
+<p class="mb-0"><strong><a aria-controls="collapseSix" aria-expanded="false" class="collapsed w-100" data-target="#collapseSix" data-toggle="collapse" type="button"> Public Transport </a></strong></p>
+</div>
+
+<div aria-labelledby="headingSix" class="collapse" data-parent="#accordion-arrive-info" id="collapseSix">
+<div class="card-body">You can use Auto-Rikshaws, Bus and Taxi Ride-Hailing services such as Uber and Ola services to reach the venue from anywhere in the city.
+</div>
+</div>
+<br>
+<div class="featured-text">
+<h3 style="text-align: center;">&nbsp;</h3>
+
+<h3 style="text-align: center;"><b>Travel Partner</b></h3>
+<div align="center"><a href="https://hechospitality.com/iide/" target="_blank"><img alt="" src="assets/images/media_partners/HEC_logo.PNG" style="width:250" /></a></div>
+
+<br>
+<br>
+<div class="text-center"><a href="https://hechospitality.com/iide/" target="_blank"><button class="btn btn-custom">Click here to book your travel plan</button></a></div>
+
+<!--<div class="text-center"><button class="btn btn-custom">Click here to book your travel plan</button></div>-->
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+</main>
+
+<?php include 'footer.php';?>
